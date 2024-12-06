a4fa7a7cae05341dd1578eadf7b2bfc215566dc3
KlinJava <alarmpinglin@gmail.com>
2024-12-06 11:32:58 +0800

    initial commit

---
+++ app/src/main/java/com/hank/bmi3/Hello.kt
@@ -0,0 +1,15 @@
+package com.hank.bmi3
+
+class Hello {
+
+}
+
+fun main() {
+    println("Hello Kotlin!")
+    val age:Int = 20
+    println(age.inc())
+    var name = "Hank"
+    println(name.get(1))
+    name = "Jack"
+}
+
