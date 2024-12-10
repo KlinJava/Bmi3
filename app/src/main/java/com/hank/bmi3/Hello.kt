c3a0fab3ce9446c4bb66f581b86d5fd8f914ffc9
KlinJava <alarmpinglin@gmail.com>
2024-12-10 16:10:31 +0800

    constructor

--- app/src/main/java/com/hank/bmi3/Hello.kt
+++ app/src/main/java/com/hank/bmi3/Hello.kt
@@ -6,9 +6,23 @@ class Hello {
 
 fun main() {
 //    dataType()
-    val p = Person()
+    val p = Person("Jack",65.5f,17f)
+    println(p.bmi())
+    val hank = Person("Hank",70f,1.75f)
+    println("${p.name} ${p.bmi()}")
+    println("${hank.name} ${hank.bmi()}")
+    p.check()
     p.hello()
+    //BMI
+    val w = 65.5f
+    val h = 1.7f
+    val bmi = w / (h * h)
+    println("BMI:$bmi")
 
+}
+
+fun Person.check(){
+    println("$name $weight $height")
 }
 
 private fun dataType() {
--- app/src/main/java/com/hank/bmi3/Person.kt
+++ app/src/main/java/com/hank/bmi3/Person.kt
@@ -1,6 +1,11 @@
 package com.hank.bmi3
 
-class Person {
+class Person constructor(val name:String, val weight:Float, val height:Float) {
+    fun bmi():Float {
+        val bmi = weight / (height * height)
+        return bmi
+    }
+
     fun hello() {
         println("Hello")
     }
